01d48c3cff7859c1bcc641732137c00c54cb8664
Shanudha tirosh <147226501+Tirosh1@users.noreply.github.com>
2024-10-14 20:08:06 +0530

    Delete updated-script-js (2).js

--- updated-script-js (2).js
+++
@@ -1,4 +0,0 @@
-// App data
-const apps = [
-    { name: 'OneDrive', url: 'https://onedrive.live.com', icon: 'fa-brands fa-microsoft' },
-    { name: 'Blackbox AI', url: 'https://www.useblackbox.io', icon: 'fa-
