14ef0a5c13de937a8a45a622fb921eeb93fef7b4
Shanudha tirosh <147226501+Tirosh1@users.noreply.github.com>
2024-10-14 20:04:35 +0530

    Add files via upload

---
+++ updated-script-js (2).js
@@ -0,0 +1,4 @@
+// App data
+const apps = [
+    { name: 'OneDrive', url: 'https://onedrive.live.com', icon: 'fa-brands fa-microsoft' },
+    { name: 'Blackbox AI', url: 'https://www.useblackbox.io', icon: 'fa-
